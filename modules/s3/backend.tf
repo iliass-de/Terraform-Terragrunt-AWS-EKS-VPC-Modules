ccbe7bc2596a627657391bfaebaf4400ef079fca
Iliass Hilmi <iliasshlm@gmail.com>
2020-04-17 20:27:10 +0200

    update repo

---
+++ modules/s3/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {
+    bucket = var.bucket
+    key    = var.key
+    region = var.region
+  }
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,15 @@
+variable "bucket" {
+  description = "bucker name"
+  type = string
+}
+
+variable "key" {
+  description = "key"
+  type        = string
+}
+
+variable "region" {
+   description = "region"
+   type        = string
+}
+
